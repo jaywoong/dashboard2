e57a2f1630759d9749f5fb30bfaf527c13d38137
yoojunwoong <yoojunwoong3@gmail.com>
2021-07-28 22:15:10 +0900

    final_YJW

--- dashboard2/views.py
+++ dashboard2/views.py
@@ -96,6 +96,162 @@ def health11(request):
     };
     return render(request, 'index.html',context);
 
+def region01(request):
+    context = {
+        'section':'region01.html',
+    };
+    return render(request, 'index.html',context);
+
+def region02(request):
+    context = {
+        'section':'region02.html',
+    };
+    return render(request, 'index.html',context);
+
+def region03(request):
+    context = {
+        'section':'region03.html',
+    };
+    return render(request, 'index.html',context);
+
+def region04(request):
+    context = {
+        'section':'region04.html',
+    };
+    return render(request, 'index.html',context);
+
+def region05(request):
+    context = {
+        'section':'region05.html',
+    };
+    return render(request, 'index.html',context);
+
+def region06(request):
+    context = {
+        'section':'region06.html',
+    };
+    return render(request, 'index.html',context);
+
+def region07(request):
+    context = {
+        'section':'region07.html',
+    };
+    return render(request, 'index.html',context);
+
+def region08(request):
+    context = {
+        'section':'region08.html',
+    };
+    return render(request, 'index.html',context);
+
+def region09(request):
+    context = {
+        'section':'region09.html',
+    };
+    return render(request, 'index.html',context);
+
+def region10(request):
+    context = {
+        'section':'region10.html',
+    };
+    return render(request, 'index.html',context);
+
+def region11(request):
+    context = {
+        'section':'region11.html',
+    };
+    return render(request, 'index.html',context);
+
+def region12(request):
+    context = {
+        'section':'region12.html',
+    };
+    return render(request, 'index.html',context);
+
+def region13(request):
+    context = {
+        'section':'region13.html',
+    };
+    return render(request, 'index.html',context);
+
+def region14(request):
+    context = {
+        'section':'region14.html',
+    };
+    return render(request, 'index.html',context);
+
+def region15(request):
+    context = {
+        'section':'region15.html',
+    };
+    return render(request, 'index.html',context);
+
+def region16(request):
+    context = {
+        'section':'region16.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2009(request):
+    context = {
+        'section':'year2009.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2010(request):
+    context = {
+        'section':'year2010.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2011(request):
+    context = {
+        'section':'year2011.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2012(request):
+    context = {
+        'section':'year2012.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2013(request):
+    context = {
+        'section':'year2013.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2014(request):
+    context = {
+        'section':'year2014.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2015(request):
+    context = {
+        'section':'year2015.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2016(request):
+    context = {
+        'section':'year2016.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2017(request):
+    context = {
+        'section':'year2017.html',
+    };
+    return render(request, 'index.html',context);
+
+def year2018(request):
+    context = {
+        'section':'year2018.html',
+    };
+    return render(request, 'index.html',context);
+
 def mlr99(request):
     context = {
         'section':'mlr99.html',
